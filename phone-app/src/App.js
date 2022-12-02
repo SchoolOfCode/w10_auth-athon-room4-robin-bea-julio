e8fd14e1f81c2ddf54f7b3d442d44d4f3321ff7e
Robin <lawn.tee.robin@gmail.com>
2022-12-02 10:13:39 +0000

    added login and logout functionality

--- phone-app/src/App.js
+++ phone-app/src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
+import LoginButton from "./components/Login";
+import LogoutButton from "./components/Logout";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
       <h1>Phone-app</h1>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
     </div>
   );
 }
